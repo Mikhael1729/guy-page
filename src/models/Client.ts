d9aab6046776869830f9654b1d09c69cf39fb389
Mikhael Santos <Mikhael1729@gmail.com>
2018-09-14 18:09:56 -0400

    feat: Added Client class

---
+++ src/models/Client.ts
@@ -0,0 +1,17 @@
+import { init } from "helpers/Init";
+
+export interface IClient {
+    id?: string;
+    name?: string;
+    email?: string;
+    lastname?: string;
+    public?: boolean;
+    birthDate?: Date;
+}
+export interface Client extends IClient { }
+
+export class Client implements Client {
+    constructor(client: Client) {
+        init<Client>(this, client);
+    }
+}
