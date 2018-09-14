333066c99979f077c3e8d51442ee406c5ba056aa
Mikhael Santos <Mikhael1729@gmail.com>
2018-09-14 15:27:44 -0400

    feat: Added home page.

---
+++ src/components/Home.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+// tslint:disable:variable-name
+
+interface IHomeProps {
+}
+
+export const Home: React.SFC<IHomeProps> = (props) => {
+  return(
+      <React.Fragment>
+          <h1>hola</h1>
+      </React.Fragment>
+  ) ;
+};
