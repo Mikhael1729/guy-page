75aa1c0d64eeda6488af99d0252330a471117a9b
Mikhael Santos <Mikhael1729@gmail.com>
2018-09-14 18:10:07 -0400

    improves

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,7 @@
 import * as React from 'react';
+import QuantityCard from "./QuantityCard";
+import { Grid } from '@material-ui/core';
+import ClientList from './ClientList';
 // tslint:disable:variable-name
 
 interface IHomeProps {
@@ -7,7 +10,29 @@ interface IHomeProps {
 export const Home: React.SFC<IHomeProps> = (props) => {
   return(
       <React.Fragment>
-          <h1>hola</h1>
+        <h1>Home</h1>
+
+        {/* Clients quantities */}
+        <Grid container={true} justify="space-between">
+            <Grid item={true} xs={6}>
+                <QuantityCard 
+                    title="My clients quantity" 
+                    backgroundColor="#E6E6E6"
+                    quantity={12}/>
+            </Grid>
+
+            <Grid item={true} xs={6}>
+                <QuantityCard 
+                    title="My clients quantity" 
+                    backgroundColor="#F0F0F0"
+                    quantity={12}/>
+            </Grid>
+        </Grid>
+
+        {/* My client list */}
+        <ClientList />
+        <ClientList />
+
       </React.Fragment>
   ) ;
 };
